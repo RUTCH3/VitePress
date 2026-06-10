2fd26e491891bf94ce970257743b1669f9b028a1
RUTCH3 <ruti3013@gmail.com>
2026-06-10 15:56:36 +0300

    :fix: Order folders in the main repo.

--- VitePress-Rut-Chizkiyahu old/.vitepress/theme/Layout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div class="my-banner">ברוכים הבאים לאתר!</div>
-    <DefaultTheme.Layout />
-  </div>
-</template>
-<script setup lang="ts">
-import DefaultTheme from 'vitepress/theme'
-</script>
